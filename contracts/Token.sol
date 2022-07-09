dcf60ccd07615ed11c94761ca31dc8d47a31d318
MB <michael.dburke56@gmail.com>
2022-07-09 10:50:24 -0500

    Implement basic erc20 contract.

---
+++ contracts/Token.sol
@@ -0,0 +1,42 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.14;
+
+contract Token {
+    event Transfer(address indexed from, address indexed to, uint256 amount);
+    event Approval(address indexed owner, address indexed spender, uint256 amount);
+
+    uint256 public totalSupply;
+    mapping(address => uint256) public balanceOf;
+    //      owner => (spender => amount)
+    mapping(address => mapping(address => uint256)) public allowance;
+    string public name = "Test token";
+    string public symbol = "TST";
+    uint8 public decimals = 18;
+
+    function transfer(address recipient, uint256 amount) external returns (bool){
+        balanceOf[msg.sender] -= amount;
+        balanceOf[recipient] += amount;
+        emit Transfer(msg.sender, recipient, amount);
+        return true;
+    }
+    
+    function approve(address spender, uint256 amount) externals returns (bool) {
+        allowance[msg.sender][spender] = amount;
+        emit Approval(msg.sender, spender, amount);
+        return true;
+    }       
+
+    function transerFrom(address sender, address recipient, uint256 amount) 
+        external returns (bool) {
+            allowance[sender][msg.sender] -= amount; 
+            // The built in underflow protection (as of solidity 8) will
+            // force this to revert if there is insuficient or no allowance 
+            // at all.
+
+            balanceOf[sender] -= amount;
+            balanceOf[recipient] += amout;
+            emit Transfer(sender, recipient, amount);
+            return true;
+    }   
+}
+
