8b6bd8bc28236e56371b7afa00bde3b271193825
Michael Burke <michael.dburke56@gmail.com>
2022-07-14 16:04:12 -0400

    Resolve nested mapping syntax issue.

--- week3/challenge_3.sol
+++ week3/challenge_3.sol
@@ -5,9 +5,24 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 contract MyToken is ERC721, ERC721URIStorage {
-    constructor() ERC721("MyToken", "MTK") {}
+    constructor() ERC721("rottenPlank", "RTP") {}
 
-    // The following functions are overrides required by Solidity.
+    event Transfer(address indexed _from, address indexed _to, uint256 indexed _tokenId);
+
+    mapping(uint256 => address) private _owners;
+    mapping(address => mapping(address => bool)) private _operators;
+    string baseURL = "";
+    uint256 nextIdToMint = 10;
+
+    function mint() external {
+        require(nextIdToMint > 0);
+        _owners[nextIdToMint] = msg.sender;
+        nextIdToMint --;
+        
+    }
+
+//----------------------------------------------------------------------------\\
+//--------------------------------Overrides------------------------------------\\
 
     function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
         super._burn(tokenId);
