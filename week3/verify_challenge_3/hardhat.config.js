5543d735e8dda20537a5dd5503faf009151e894a
MB <michael.dburke56@gmail.com>
2022-07-18 15:17:46 -0500

    Setup env var / include etherscan env for contract verification.

--- week3/verify_challenge_3/hardhat.config.js
+++ week3/verify_challenge_3/hardhat.config.js
@@ -1,6 +1,12 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
 
+
+const etherScanAPI = process.env.ETHER_SCAN;
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
+   etherscan: {
+    apiKey: "etherScanAPI"
+  }
 };
