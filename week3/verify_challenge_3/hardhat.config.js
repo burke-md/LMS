eb4a2ef9ddfc3801805f10956f55921bae083e3a
MB <michael.dburke56@gmail.com>
2022-07-18 18:25:53 -0500

    Update compiler version in hardhat config to match contract.

--- week3/verify_challenge_3/hardhat.config.js
+++ week3/verify_challenge_3/hardhat.config.js
@@ -5,9 +5,18 @@ require('dotenv').config();
 
 
 const etherScanAPI = process.env.ETHER_SCAN;
+const alchemyApiKey = process.env.ALCHEMY_API;
+const mnemonic = process.env.MNEMONIC;
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.9",
+  solidity: "0.8.15",
+  networks: {
+     rinkeby: {
+       url: `https://eth-rinkeby.alchemyapi.io/v2/${alchemyApiKey}`,
+       accounts: { mnemonic: mnemonic },
+     },
+   },
    etherscan: {
     apiKey: "etherScanAPI"
   }
