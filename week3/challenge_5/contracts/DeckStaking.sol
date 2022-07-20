d8254f8d66ece159582b7706857afd4f172e53c2
MB <michael.dburke56@gmail.com>
2022-07-20 15:12:25 -0500

    Implement stake functionality w/ observation of tokenId to prevent re-stake edge case.

--- week3/challenge_5/contracts/DeckStaking.sol
+++ week3/challenge_5/contracts/DeckStaking.sol
@@ -11,7 +11,8 @@ contract DeckStaking is IERC721Receiver {
 
     address public immutable DECK_20_TOKEN;
     address public immutable DECK_721_TOKEN;
-    
+    uint256 public timeToReward = 10; //check date formating
+
     //Create struct to store staking data
     struct StakedToken {
         address owner;
@@ -30,11 +31,31 @@ contract DeckStaking is IERC721Receiver {
 
     //User must be able to stake NFT(contract is to own NFT)
     function stake(uint256 _tokenId) external {
+
+    //Make check for edge case (restake within rewardsHoldingTime)
+    /** Point of interest: Should address or tokenId be observed to prevent 
+    *   restake for quick rewards edge case? 
+    *   
+    *   Address: New owner would not face lock out time - one owner could just
+    *   transer between wallets and restake.
+    * 
+    *   tokenId: Easily prevent this issue - new owner face unwarented lockout.
+    *   If staking rewards were significant enough, user may want access to last
+    *   staking time stamp info before purchase  of "spent" NFT.
+    *
+    */ 
+
+        if (stakes[_tokenId].timeStamp + timeToReward < block.timestamp) {
+            revert("Contract: This token has recently been staked. Please try later!");
+        }
+
+        // user needs to allow transfer form this contract address
+        DECK_721_TOKEN.safeTransferFrom(msg.sender, address(this), _tokenId);
         //Note original owner, time stamp of stake
         stakes[_tokenId] = StakedToken(msg.sender, block.timestamp);
         //Check ownership?? or will lack of ownership force reversion?
 
-    //Make check for edge case (restake within rewardsHoldingTime)
+
     }
     
     //User must be able to reclaim NFT 
