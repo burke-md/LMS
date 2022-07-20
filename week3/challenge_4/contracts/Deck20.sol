79e86b80989c2652fcae61b05b50d330144d0ca1
MB <michael.dburke56@gmail.com>
2022-07-19 19:27:02 -0500

    Define required functions (no implementation) in erc20.

--- week3/challenge_4/contracts/Deck20.sol
+++ week3/challenge_4/contracts/Deck20.sol
@@ -6,5 +6,23 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Deck20 is ERC20 {
     constructor() ERC20("rottenPlank", "RTP") {}
+    
+    function purchaseTokens(uint256 _amount) external payable {
+
+
+    }
+
+    function withdrawl(uint256 _amount) external onlyAdmin { 
+
+
+    }
+
+    receive() external payable {
+        revert("Contract: Use 'purchaseTokens' functions to aquire tokens.");
+    }
+
+    fallback() external payable {
+        revert("Contract: Use appropriate function for desired outcomes.");
+    }
 
 }
