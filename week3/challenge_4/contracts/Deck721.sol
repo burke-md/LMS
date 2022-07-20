dd78df4ceb764894334128a0b2507db656ed2767
MB <michael.dburke56@gmail.com>
2022-07-19 19:32:08 -0500

    Define required funcs for erc721.

--- week3/challenge_4/contracts/Deck721.sol
+++ week3/challenge_4/contracts/Deck721.sol
@@ -5,7 +5,21 @@ pragma solidity 0.8.15;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract Deck20 is ERC721 {
-    constructor() ERC721("rottenPlank", "RTP") {}
+    constructor() ERC721("rottenPlank", "RTP") {
+    
+        adminAddress = msg.sender; 
+    }
+
+    address immutable adminAddress;
+    address constant erc20Address = "";
+
+    function mint() external payable {
+
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "ipfs://Qm/";
+    }
 
 }
 
