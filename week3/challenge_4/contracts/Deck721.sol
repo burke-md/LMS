0582d7747aeeea0cde9ed4dd89a459f3057afc53
MB <michael.dburke56@gmail.com>
2022-07-19 19:18:47 -0500

    Set up hardhat project to compile and set up challenge 4 proj.

---
+++ week3/challenge_4/contracts/Deck721.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.15;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract Deck20 is ERC721 {
+    constructor() ERC721("rottenPlank", "RTP") {}
+
+}
+
