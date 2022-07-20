c44454db7a687262e8686a6175100ca27937e215
MB <michael.dburke56@gmail.com>
2022-07-20 10:57:11 -0500

    Update function implementation in deck20 token.

--- week3/challenge_4/contracts/Deck20.sol
+++ week3/challenge_4/contracts/Deck20.sol
@@ -9,22 +9,36 @@ contract Deck20 is ERC20 {
         adminAddress = msg.sender; 
     }
 
-    address adminAddress;
+    address private adminAddress;
+    uint256 private constant tokenPrice = 0;
 
     modifier onlyAdmin() {
         require(msg.sender == adminAddress, 
                 "Contract: This function is only available to the deployer.");
         _;
     }
-    
-    function purchaseTokens(uint256 _amount) external payable {
+   
+    /** This is an open ended, free token.
+    *   
+    *   The tokens cost 0 Ether (see tokenPrice var above).
+    * 
+    *   There is no upper bound on how many can be purchased. 
+    *
+    */
 
+    function purchaseTokens(uint256 _amount) external payable {
+        require(msg.value == _amount * tokenPrice,
+                "Contract: Incorect amount of Ether sent.");
+
+        _mint(msg.sender, _amount);
 
     }
 
     function withdrawl(uint256 _amount) external onlyAdmin { 
+        require(msg.sender == adminAddress, 
+                "Contract: This function is only available to the deployer.");
 
-
+        payable(adminAddress).transfer();
     }
 
     receive() external payable {
