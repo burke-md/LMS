f0918488a2223e1dc638b25d5429af108e6175ff
MB <michael.dburke56@gmail.com>
2022-07-20 08:34:57 -0500

    Partially implement mint func.

--- week3/challenge_4/contracts/Deck721.sol
+++ week3/challenge_4/contracts/Deck721.sol
@@ -5,6 +5,7 @@ pragma solidity 0.8.15;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract Deck20 is ERC721 {
+
     constructor() ERC721("rottenPlank", "RTP") {
     
         adminAddress = msg.sender; 
@@ -12,14 +13,20 @@ contract Deck20 is ERC721 {
 
     address immutable adminAddress;
     address constant erc20Address = "";
+    uint256 private nextIdToMint = 10;
 
     function mint() external payable {
+        uint256 currentIdIndex = nextIdToMint;
+        require(currentIdIndex > 0, 
+                "Contract: Minting would exceed max supply.");
+        // handle erc20 payable
+        _mint(msg.sender, currentIdIndex);
+        nextIdToMint--;
 
     }
 
     function _baseURI() internal pure override returns (string memory) {
         return "ipfs://Qm/";
     }
-
 }
 
