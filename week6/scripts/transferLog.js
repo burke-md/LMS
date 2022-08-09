ec27ccd374a6a98c25ef7fe90be36b930b68ebe4
MB <michael.dburke56@gmail.com>
2022-08-09 15:01:26 -0500

    Import ethers and receive transactions from tether contract.

--- week6/scripts/transferLog.js
+++ week6/scripts/transferLog.js
@@ -1,2 +1,16 @@
-require('dotenv').config();
-const alchemyApiKey = process.env.ALCHEMY_API;
+import { Alchemy, Network } from "alchemy-sdk";
+import { ethers } from "ethers";
+
+const alchemy = new Alchemy();
+const filter = {
+    address: "0xdAC17F958D2ee523a2206206994597C13D831ec7",
+    topics: [
+        ethers.utils.id("Transfer(address,address,uint256)")
+    ]
+}
+
+alchemy.ws.on(filter, (log, event)  => {
+    console.log(`log: ${JSON.stringify(log)}`);
+});
+
+
