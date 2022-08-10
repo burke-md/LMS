a514ce7c8f8a310c2d808c93aa48c55a76063770
MB <michael.dburke56@gmail.com>
2022-08-10 07:02:19 -0500

    Implement transferLog script.

--- week6/scripts/transferLog.js
+++ week6/scripts/transferLog.js
@@ -2,15 +2,25 @@ import { Alchemy, Network } from "alchemy-sdk";
 import { ethers } from "ethers";
 
 const alchemy = new Alchemy();
-const filter = {
-    address: "0xdAC17F958D2ee523a2206206994597C13D831ec7",
-    topics: [
-        ethers.utils.id("Transfer(address,address,uint256)")
-    ]
-}
 
-alchemy.ws.on(filter, (log, event)  => {
-    console.log(`log: ${JSON.stringify(log)}`);
-});
+async function main() {
+    const result = await alchemy.core
+        .getLogs({
+            address: "0xdAC17F958D2ee523a2206206994597C13D831ec7",
+            topics: [
+                ethers.utils.id("Transfer(address,address,uint256)")
+            ]
+        });
+    
+    const data = {
+        blockNumber: result[0].blockNumber,
+        numberOfTx: result.length
+    }
 
+    return data;
+};
+
+
+const blockStats = await main();
+console.log(blockStats);
 
