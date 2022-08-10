b559dd78d3c34a705e0d99c733d96b865c05e134
MB <michael.dburke56@gmail.com>
2022-08-10 11:09:55 -0500

    Implement baseFeed.

--- week6/scripts/basefeeLog.js
+++ week6/scripts/basefeeLog.js
@@ -1,2 +1,22 @@
-require('dotenv').config();
-const alchemyApiKey = process.env.ALCHEMY_API;
+import { Alchemy, Network } from "alchemy-sdk";
+import { ethers } from "ethers";
+
+const alchemy = new Alchemy();
+
+async function main() {
+    const result = await alchemy.core
+        .getBlock()
+   /* 
+    const data = {
+        blockNumber: result[0].blockNumber,
+        numberOfTx: result.length
+    }
+
+    return data;
+    */
+    return result.baseFeePerGas.toString();
+};
+
+
+const blockStats = await main();
+console.log(blockStats);
