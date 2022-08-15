1753f12283f2b1b1b2ee536be061ffa47f23398a
Michael Burke <michael.dburke56@gmail.com>
2022-08-15 16:24:00 -0400

    Resolve data bug in baseFee chart.

---
+++ week6/charts/pages/basefeeChart.js
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import useInterval from "./hooks/useInterval";
+import LineChart from "./components/LineChart";
+import fetchBaseFee from "./api/fetchBaseFee";
+
+async function fetchChartData() {
+    const newBaseFeeData = await fetchBaseFee();
+
+    return {
+        blockNumber: newBaseFeeData.blockNumber,
+        baseFee: newBaseFeeData.baseFee
+    }
+}
+
+
+
+export default function OnChainData() {
+    let [blockNumbers, setBlockNumbers] = useState(['0']);
+    let [baseFeeData, setBaseFeeData] = useState([0]);
+
+
+    useInterval(async () => {
+        const currentBlockNum = blockNumbers.slice(-1);
+        const newData = await fetchChartData();
+
+        if(newData.blockNumber == currentBlockNum ||
+            newData.blockNumber == undefined){
+            console.log(`Invalid block`);
+            return; //Do not add new data to state -> continue to poll new data
+        }
+
+        setBlockNumbers([...blockNumbers, newData.blockNumber]);
+        setBaseFeeData([...baseFeeData, newData.baseFee]);
+
+    }, 3000)
+
+    return (
+        <LineChart 
+            xAxisElementArray={blockNumbers}
+            lineZeroDataArray={baseFeeData}
+            chartName={"Basefee"} 
+            lineName={"Basefeee (wei)"}
+        />
+    )
+};
