21b13dc55a3baadb69acbcf81d5c7eb7c5418a73
Michael Burke <michael.dburke56@gmail.com>
2022-08-17 09:10:50 -0400

    Work out 'look back' fetching.

--- week6/charts/pages/tetherTxChart.js
+++ week6/charts/pages/tetherTxChart.js
@@ -3,37 +3,83 @@ import useInterval from "./hooks/useInterval";
 import LineChart from "./components/LineChart";
 import fetchTransferLog from "./api/fetchTransferLog";
 
-async function fetchChartData() {
-    const newGasRatioData = await fetchTransferLog();
-    return {
-        blockNumber: newGasRatioData.blockNumber,
-        numberOfTx: newGasRatioData.numberOfTx
-    }
-}
-
 export default function tetherTxChart() {
-    let [blockNumbers, setBlockNumbers] = useState(['0']);
-    let [tetherTxData, setTetherTxData] = useState([0]);
+    let [blockNumbers, setBlockNumbers] = useState([]);
+    let [txData, setTxData] = useState([]);
 
     useInterval(async () => {
-        const currentBlockNum = blockNumbers.slice(-1);
-        const newData = await fetchChartData();
+        const xAxisLength = blockNumbers.length;
+        let targetBlock;
+        // three stages:
+        // X first render
+        // less than ten
+        // greater than ten
 
-        if(newData.blockNumber == currentBlockNum ||
-            newData.blockNumber == undefined){
-            console.log(`Invalid block`);
+        if(xAxisLength < 1) {
+            targetBlock = null;
+        }
+
+        if(xAxisLength >= 1 && xAxisLength <= 10) {
+            //
+            let z = blockNumbers;
+            let x = blockNumbers[0];
+            console.log(`xAxis length: ${xAxisLength}`)
+            console.log(`full block num array: ${z}`)
+            console.log(`first index: ${x}`)
+            //
+            targetBlock = Number(blockNumbers[0]) - 1;
+            console.log(`target block: ${targetBlock}`)
+        }
+
+        if(xAxisLength > 10) {
+            targetBlock = Number(blockNumbers.slice(-1)) + 1;
+            console.log(`target block: ${targetBlock}`)
+        }        
+
+        const newData = await fetchTransferLog(targetBlock);
+
+        //Clean data
+
+        if(!newData){
+            console.log(`Invalid response.`);
             return; //Do not add new data to state -> continue to poll new data
         }
 
-        setBlockNumbers([...blockNumbers, newData.blockNumber]);
-        setTetherTxData([...tetherTxData, newData.numberOfTx]);
+        //TODO make check for repeat data??
 
+        let cleanedBlockNumbersArr;
+        let cleanedTxDataArr;
+
+        if(xAxisLength === 0) {
+            cleanedBlockNumbersArr = [newData.blockNumber];
+            cleanedTxDataArr = [newData.numberOfTx];
+        }
+
+        if(xAxisLength > 0 && xAxisLength < 11) {
+            cleanedBlockNumbersArr = [newData.blockNumber, ...blockNumbers];  
+            cleanedTxDataArr = [newData.numberOfTx, ...txData];
+        }
+
+        if(xAxisLength > 10) {
+            cleanedBlockNumbersArr = [...blockNumbers, newData.blockNumber];
+            cleanedTxDataArr = [...txData, newData.numberOfTx];
+        }
+
+        console.log(`right before setting state`);
+        console.log(`BN.len: ${cleanedBlockNumbersArr.length}`);
+        console.log(`TX.len: ${cleanedTxDataArr.length}`);
+        console.log(`===`)
+
+        setBlockNumbers([...cleanedBlockNumbersArr]);
+        setTxData([...cleanedTxDataArr]);
     }, 3000)
+
+
 
     return (
         <LineChart 
             xAxisElementArray={blockNumbers}
-            lineZeroDataArray={tetherTxData}
+            lineZeroDataArray={txData}
             chartName={"Tether transfer logs"} 
             lineName={"Number of transfers"}
         />
