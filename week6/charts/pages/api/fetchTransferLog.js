d78f03847a78733a55a8c321fdcecfbc8f754053
Michael Burke <michael.dburke56@gmail.com>
2022-08-17 09:10:32 -0400

    Add try/catch block and lint.

--- week6/charts/pages/api/fetchTransferLog.js
+++ week6/charts/pages/api/fetchTransferLog.js
@@ -3,23 +3,31 @@ import { ethers } from "ethers";
 
 const alchemy = new Alchemy();
 
-const makeRequest = async function() {
-    const res = await alchemy.core
+const makeRequest = async function(blockNum) {
+    let res = null;
+    try {
+        res = await alchemy.core
         .getLogs({
             address: "0xdAC17F958D2ee523a2206206994597C13D831ec7",
+            //Where setting to & from to same value returns one block's data
+            //& passing in null values returns current block.
+            fromBlock: blockNum,
+            toBlock: blockNum,
             topics: [
                 ethers.utils.id("Transfer(address,address,uint256)")
             ]
         });
+    } catch (err) {
+        console.log(`===\ngetLogs w/ blockNum: ${blockNum} has thrown the following error: \n ${err}`);
+    }
     return res;
 }
 
-export default async function fetchTransferLog() {
-    let result = await makeRequest();
-
-    if(result[0] == undefined) {
-        console.log(`bad result`)
-        return -1;
+export default async function fetchTransferLog(blockNum) {
+    const result = await makeRequest(blockNum);
+    if(result == null ||
+        result == undefined) {
+        return null;
     }
 
     const data = {
