3f84fca53eae2e8520d0cd97c7e2f82a881f0cf0
Michael Burke <michael.dburke56@gmail.com>
2022-09-09 08:07:08 -0400

    Handle 1155 related imports for contract.

--- week5/contracts/TradingRocks.sol
+++ week5/contracts/TradingRocks.sol
@@ -1,8 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.16;
 
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/token/ERC1155/IERC1155Receiver.sol";
 import "hardhat/console.sol";
 
-contract TradingRocks {
+contract TradingRocks is ERC1155, IERC1155Receiver {
    
 }
