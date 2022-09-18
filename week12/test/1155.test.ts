b9db55891a5d5935686cb4a302225c05220ec637
MB <michael.dburke56@gmail.com>
2022-09-18 14:36:40 -0500

    Clean demo project and set up for 1155 YUL proj.

--- week12/test/1155.test.ts
+++ week12/test/1155.test.ts
@@ -2,7 +2,7 @@ import { time, loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
 import { expect } from "chai";
 import { ethers } from "hardhat";
-
+/*
 describe("Lock", function () {
   // We define a fixture to reuse the same setup in every test.
   // We use loadFixture to run this setup once, snapshot that state,
@@ -122,3 +122,5 @@ describe("Lock", function () {
     });
   });
 });
+
+*/
